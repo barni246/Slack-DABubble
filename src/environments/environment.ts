8f4011ee4d2fbe3f4687d2c0fde85854d4adc629
Barnabas Gonda <barnabasgonda@Barnabass-iMac.local>
2023-08-30 19:47:57 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'test-9642e',
+    appId: '1:212903323064:web:178e5e8768a0ed7b589b4d',
+    storageBucket: 'test-9642e.appspot.com',
+    locationId: 'europe-west3',
+    apiKey: 'AIzaSyCIDoIH0FkLTZnfRQ4-LIQk68rkfqdLtGs',
+    authDomain: 'test-9642e.firebaseapp.com',
+    messagingSenderId: '212903323064',
+    measurementId: 'G-8PRRFQPD39',
+  },
+};
